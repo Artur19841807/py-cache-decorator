62ab88f175e5bb7b0fd7cfd95e5e846d78fb908f
Artur19841807 <131347306+Artur19841807@users.noreply.github.com>
2026-06-11 09:56:36 +0300

    Fix cache decorator function definition

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
 
-def cache(func):
+
+def cache(func: Callable[..., Any]) -> Callable[..., Any]:
     results_cache = {}
 
-    def inner(*args, **kwargs):
+    def inner(*args: Any, **kwargs: Any) -> Any:
         cache_key = (args, tuple(kwargs.items()))
-        
+
         if cache_key not in results_cache:
             print("Calculating new result")
             results_cache[cache_key] = func(*args, **kwargs)
         else:
             print("Getting from cache")
-            
+
         return results_cache[cache_key]
 
     return inner
