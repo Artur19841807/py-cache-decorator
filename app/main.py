2394700345fa930a95722b6c1140bb12abfdc34e
Artur19841807 <131347306+Artur19841807@users.noreply.github.com>
2026-06-11 09:23:25 +0300

    Add  cache decorator
    Implement a caching decorator to store results of function calls.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from typing import Callable
 
-
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    results_cache = {}
+
+    def inner(*args, **kwargs):
+        cache_key = (args, tuple(kwargs.items()))
+        
+        if cache_key not in results_cache:
+            results_cache[cache_key] = func(*args, **kwargs)
+            
+        return results_cache[cache_key]
+
+    return inner
